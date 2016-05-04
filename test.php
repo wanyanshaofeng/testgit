10390411aa6c7a3d241040e8a03ef8ed38782e39
wanyanshaofeng <wanyanshaofeng@163.com>
2016-05-04 10:12:18 +0800

    test.php 创建和修改

---
+++ test.php
@@ -0,0 +1,38 @@
+<?php 
+ $con=mysql_connect('localhost','root','');
+ if($con){
+   echo "connect ok !";
+ }else{
+    echo "connect fail ! ";
+ }
+  
+  class demo{
+   private $username=root";
+   private $password="";
+   private $host="localhost";
+
+   function __construct(){
+      $this-> connectsql($this->host,$this->username,$this->password);
+      $this->mysqlclose();
+           
+    }
+
+    protected function  connectsql($host,$username,$pwd){
+      $con=mysql_connect($host,$username,$pwd);
+      if($con){
+         echo "数据库连接成功！";
+       }else{
+         echo  "数据库连接失败！";
+       }
+    
+    
+    }
+    protected function mysqlclose(){
+       mysql_close();
+    
+    }
+  
+  
+  }
+
+ ?>
